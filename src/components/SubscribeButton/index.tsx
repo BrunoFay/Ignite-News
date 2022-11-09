ff266cc53ff419fc76165544210c871c794995e5
bruno <brunofay1@hotmail.com>
2022-11-09 17:03:19 -0300

    NEW add stripe request on static method

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './styles.module.scss'
 
-export default function SubscribeButton() {
+export default function SubscribeButton({ priceId }: { priceId: string }) {
   return <button className={styles.subscribeButton}>Subscribe now</button>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import SubscribeButton from '../components/SubscribeButton'
+import { stripe } from '../libs/stripe'
 import ManImage from '../public/images/manHomePage.svg'
 import styles from '../styles/home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: string
+  }
+}
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,12 +26,30 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9,90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <Image src={ManImage} alt="Man coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1M2K0ECE9RrEGdXN0uLeWIK0', {
+    expand: ['product'],
+  })
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-us', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount! / 100),
+  }
+  return {
+    props: {
+      product,
+    },
+  }
 }
