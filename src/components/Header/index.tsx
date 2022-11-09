b5ddc6ca5235502136b4cb8e7a8ca06b05e1c733
bruno <brunofay1@hotmail.com>
2022-11-09 14:53:02 -0300

    NEW start header componente

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import Logo from '../../public/images/logo.svg'
+import styles from './styles.module.scss'
+
+export default function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src={Logo} alt="ig news" />
+        <nav>
+          <Link className={styles.active} href="#">
+            Home
+          </Link>
+          <Link href="#">Post</Link>
+        </nav>
+      </div>
+    </header>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { Roboto } from '@next/font/google'
 
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Header from '../components/Header'
 
 const roboto = Roboto({
   weight: ['400', '700', '900'],
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
           font-family: ${roboto.style.fontFamily};
         }
       `}</style>
+      <Header />
       <Component {...pageProps} />
     </>
   )
