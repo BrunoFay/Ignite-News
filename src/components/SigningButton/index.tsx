c3b6556c5f6ccfaa9d8000c593026275a6eb46a0
bruno <brunofay1@hotmail.com>
2022-11-10 12:16:46 -0300

    NEW add next auth and configured it

--- src/components/SigningButton/index.tsx
+++ src/components/SigningButton/index.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
+import { useSession, signIn } from 'next-auth/react'
 
 export default function SigningButton() {
-  const isUserLoggedIn = false
-  return isUserLoggedIn ? (
+  const { data: session } = useSession()
+  return session ? (
     <button className={styles.singInButton}>
       <FaGithub color="#04d361" />
       Bruno fay
       <FiX className={styles.closeIcon} color="#737380" />
     </button>
   ) : (
-    <button className={styles.singInButton}>
+    <button onClick={() => signIn()} className={styles.singInButton}>
       <FaGithub color="#A717EB" />
       Sign in with Github
     </button>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unknown-property */
 import { Roboto } from '@next/font/google'
-
+import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Header from '../components/Header'
@@ -9,9 +9,12 @@ const roboto = Roboto({
   weight: ['400', '700', '900'],
 })
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <>
+    <SessionProvider session={session}>
       <style jsx global>{`
         html {
           font-family: ${roboto.style.fontFamily};
@@ -19,6 +22,6 @@ export default function App({ Component, pageProps }: AppProps) {
       `}</style>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
