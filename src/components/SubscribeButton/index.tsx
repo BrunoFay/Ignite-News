6055bfb1bf43cf8c579ba4bba80851f9c846b4ce
bruno <brunofay1@hotmail.com>
2022-11-20 14:19:10 -0300

    NEW add checkout session on subcribe button

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,17 @@
+import { useSession, signIn } from 'next-auth/react'
 import React from 'react'
 import styles from './styles.module.scss'
 
 export default function SubscribeButton({ priceId }: { priceId: string }) {
-  return <button className={styles.subscribeButton}>Subscribe now</button>
+  const { data: session } = useSession()
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+    }
+  }
+  return (
+    <button onClick={handleSubscribe} className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  )
 }
